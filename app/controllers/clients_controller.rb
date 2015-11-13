3e0281cc0a5b2e59d76c2f2a54523b752cde2c67
Martin Granados <margrangr@gmail.com>
2015-11-13 14:37:02 -0600

    dirty stuff

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -47,7 +47,7 @@ class ClientsController < ApplicationController
         to client_here.email
         from 'no-reply@syrat.mx'
         subject 'Avance en tu reparación'
-        body "Buen día  #{client_here.name}! Sigue el proceso de tu reparación aquí: https://pacific-shore-4043.herokuapp.com/clients/#{client_here.id}/ "
+        body "Buen día  #{client_here.name}! Tú código de avances es: #{client_here.unique_repair_id} \b Sigue el proceso de tu reparación aquí: https://pacific-shore-4043.herokuapp.com/clients/search?utf8=✓&id_query=#{client_here.unique_repair_id} "
       end
     else
     end
